ec538f7862e5307765dca065044cea97cb66e915
Harsh Patel <harshnpatel9@gmail.com>
2024-03-03 21:16:32 +0530

    feat: acc panel

---
+++ src/commands/setup-acc-panel.ts
@@ -0,0 +1,57 @@
+import {
+  ActionRowBuilder,
+  ButtonBuilder,
+  ButtonStyle,
+  ChannelType,
+  EmbedBuilder,
+  PermissionFlagsBits,
+  Permissions,
+  SlashCommandBuilder,
+  SlashCommandChannelOption,
+} from "discord.js";
+import { Command } from "../interface";
+
+export default {
+  data: new SlashCommandBuilder()
+    .setName("setup-account-panel")
+    .setDescription("This will setup the embeds")
+    .addChannelOption((option: SlashCommandChannelOption) => {
+      return option
+        .setName("channel")
+        .setDescription("Sélectionnez un canal dans lequel configurer le panneau du compte.")
+        .setRequired(true)
+        .addChannelTypes(ChannelType.GuildText, ChannelType.GuildAnnouncement);
+    })
+    .setDMPermission(false)
+    .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
+
+  async execute(interaction) {
+    if (!interaction.guild) return;
+    await interaction.deferReply({ ephemeral: false });
+    const channelId = (interaction.options.getChannel("channel")?.id || interaction.channelId) as string;
+    const channel = interaction.guild.channels.cache.get(channelId);
+    if (channel?.type !== ChannelType.GuildText) {
+      await interaction.reply({
+        content: "Cette commande ne peut être utilisée que dans un canal texte.",
+        ephemeral: true,
+      });
+      return;
+    }
+
+    const embed = new EmbedBuilder()
+      .setTitle("Configuration du compte")
+      .setDescription("Commençons par enregistrer ton pseudo Apex et ta plateforme.")
+      .setColor("Green")
+      .setTimestamp();
+
+    const Buttons = [
+      new ButtonBuilder().setCustomId("apex-nick").setStyle(ButtonStyle.Secondary).setLabel("Add Psd"),
+      new ButtonBuilder().setCustomId("apex-platform").setStyle(ButtonStyle.Secondary).setLabel("Add Platforme"),
+    ];
+
+    const actionRow = new ActionRowBuilder<ButtonBuilder>().addComponents(Buttons);
+    await channel.send({ embeds: [embed], components: [actionRow] });
+
+    await interaction.editReply({ content: "La configuration du panneau de compte est terminée" });
+  },
+} as Command;
