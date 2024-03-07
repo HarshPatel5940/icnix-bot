9a8c023ec9d77ecbecad57d6a83746b1db5c0d26
Harsh Patel <harshnpatel9@gmail.com>
2024-03-07 22:26:29 +0530

    feat: add update-match

---
+++ src/commands/apex/match/update-match.ts
@@ -0,0 +1,104 @@
+import {
+  ActionRowBuilder,
+  ButtonBuilder,
+  ButtonStyle,
+  ChatInputCommandInteraction,
+  Colors,
+  EmbedBuilder,
+  SlashCommandBuilder,
+} from "discord.js";
+import { Command } from "../../../interface";
+import db from "../../../utils/database";
+import { Match } from "../../../types/match";
+
+export default {
+  data: new SlashCommandBuilder()
+    .setName("update-match")
+    .setDescription("Mettre à jour les détails d'un match en cours")
+    .setDMPermission(false)
+    .addStringOption(option =>
+      option
+        .setName("match-id")
+        .setDescription("Entrez l'ID de correspondance avec lequel vous souhaitez effectuer la mise à jour")
+        .setRequired(true),
+    ) as SlashCommandBuilder,
+
+  async execute(interaction: ChatInputCommandInteraction) {
+    if (!interaction.guild) return;
+    const matchID = interaction.options.getString("match-id");
+    if (!matchID || !matchID.startsWith("mid_")) {
+      await interaction.reply({
+        content: "L'ID de correspondance fourni n'est pas valide",
+      });
+      return;
+    }
+    await interaction.deferReply({ ephemeral: true });
+
+    let data = await (await db()).collection<Match>("matches").findOne({ matchId: matchID });
+    if (!data) {
+      await interaction.editReply({
+        content: "Aucune correspondance n'a été trouvée avec l'ID de correspondance fourni",
+      });
+      return;
+    }
+
+    const embed = new EmbedBuilder()
+      .setTitle("Match Details")
+      .setDescription(`MatchID: \`${data.matchId}\`\nMatch Played at: \`${data.playedAt}\``)
+      .addFields(
+        {
+          name: "Player 1",
+          value: data.player1_name,
+          inline: true,
+        },
+        {
+          name: "Player 2",
+          value: data.player2_name,
+          inline: true,
+        },
+      )
+      .addFields(
+        {
+          name: "Completed ?",
+          value: data.isCompleted ? "Yes" : "No",
+          inline: true,
+        },
+        {
+          name: "Draw ?",
+          value: data.isDraw ? "Yes" : "No",
+          inline: true,
+        },
+        {
+          name: "Accepted By Both ?",
+          value: data.isAcceptedByP1 && data.isAcceptedByP2 ? "Match Accepted By Both" : "Not Yet Accepted By Both",
+          inline: true,
+        },
+      )
+      .setColor(Colors.Blurple);
+
+    const actionRow = new ActionRowBuilder<ButtonBuilder>().addComponents(
+      new ButtonBuilder()
+        .setCustomId(`mu-+-${data.player1_ID}-+-${data.player1_name}-+-${matchID}`)
+        .setLabel("Declare P1 Winner")
+        .setStyle(ButtonStyle.Primary),
+      new ButtonBuilder()
+        .setCustomId(`mu-+-${data.player2_ID}-+-${data.player2_name}-+-${matchID}`)
+        .setLabel("Declare P2 Winner")
+        .setStyle(ButtonStyle.Primary),
+      new ButtonBuilder()
+        .setCustomId(`mu-+-draw-+-draw-+-${matchID}`)
+        .setLabel("Declare Draw")
+        .setStyle(ButtonStyle.Secondary),
+
+      new ButtonBuilder()
+        .setLabel("View Log Msg")
+        .setStyle(ButtonStyle.Link)
+        .setURL(`https://discord.com/channels/${interaction.guildId}/${data.matchMsgChannel}/${data.matchMsgId}`),
+    );
+
+    await interaction.editReply({
+      embeds: [embed],
+      components: [actionRow],
+    });
+  },
+} as Command;
---
+++ src/events/match-cmds/handleUpdateMatch.ts
@@ -0,0 +1,107 @@
+import { Colors, EmbedBuilder, Events, Interaction } from "discord.js";
+import db from "../../utils/database";
+import { Match } from "../../types/match";
+
+export default {
+  name: Events.InteractionCreate,
+  once: false,
+
+  async execute(interaction: Interaction) {
+    if (!interaction.isButton()) return;
+    if (!interaction.guild) return;
+    if (!interaction.customId.startsWith("mu-+-")) return;
+    await interaction.deferReply({ ephemeral: false });
+
+    const [declare, declareId, declareName, matchId] = interaction.customId.split("-+-");
+
+    if (!declareId || !matchId) {
+      console.log(declareId, declareName, matchId);
+      await interaction.reply({
+        content: "Incomplete button ID. Something went wrong!",
+        ephemeral: true,
+      });
+      return;
+    }
+
+    let match;
+    if (declare !== "draw") {
+      match = await (await db()).collection<Match>("matches").findOneAndUpdate(
+        { matchId: matchId },
+        {
+          $set: {
+            winner_ID: declareId,
+            winner_Name: declareName,
+            isCompleted: true,
+            updatedAt: new Date(),
+          },
+        },
+        {
+          upsert: false,
+          returnDocument: "after",
+        },
+      );
+    } else {
+      match = await (await db()).collection<Match>("matches").findOneAndUpdate(
+        { matchId },
+        {
+          $set: {
+            winner_ID: declareId,
+            winner_Name: declareName,
+            isDraw: true,
+            isCompleted: true,
+            updatedAt: new Date(),
+          },
+        },
+        {
+          upsert: false,
+          returnDocument: "after",
+        },
+      );
+      console.log(match);
+    }
+
+    if (!match?._id) {
+      await interaction.editReply({
+        content: "Match not updated!",
+      });
+      return;
+    }
+
+    // isCompleted ? match.winner_Name ? "Draw" : "Not Yet Declared"
+    let winner = "None";
+    if (match.isDraw) {
+      winner = "Draw";
+    } else {
+      winner = match.winner_Name ? match.winner_Name : "Not Yet Declared";
+    }
+
+    const embed = new EmbedBuilder()
+      .setTitle("Match Details Updated")
+      .setDescription(
+        `MatchID: \`${match.matchId}\`\nMatch Played at: \`${match.playedAt}\`\nUpdated By: <@${interaction.user.id}>`,
+      )
+      .addFields(
+        {
+          name: "Player 1",
+          value: match.player1_name,
+          inline: true,
+        },
+        {
+          name: "Player 2",
+          value: match.player2_name,
+          inline: true,
+        },
+        {
+          name: "Winner",
+          value: winner,
+          inline: true,
+        },
+      )
+      .setColor(Colors.Green)
+      .setTimestamp();
+
+    await interaction.editReply({
+      embeds: [embed],
+    });
+  },
+};
